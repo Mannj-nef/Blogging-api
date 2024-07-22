d1b95af258f43b96bad89ed196fa8e16caf9c017
Mannj-nef <manhquan.05012002@gmail.com>
2024-07-22 20:39:15 +0700

    init source base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { UserModule } from './user/user.module'
+import { TopicModule } from './topic/topic.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '',
+      port: 5432,
+      username: '',
+      password: '',
+      database: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
+    UserModule,
+    TopicModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
